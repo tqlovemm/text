7e1531e96e40207735fea8c4947b19d8839534e5
tqlmm <397114202@qq.com>
2017-04-06 16:26:50 +0800

    master

---
+++ model/SiteController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/6
+ * Time: 16:20
+ */
+class SiteController{
+
+    public function actionIndex(){
+
+        echo "site-index";
+
+    }
+
+}
