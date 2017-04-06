87984bdd1177693f625bbc7c668522137d0f39f3
tqlmm <397114202@qq.com>
2017-04-06 16:49:08 +0800

    master

---
+++ model/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/6
+ * Time: 16:20
+ */
+class DefaultController{
+
+    public function actionIndex(){
+
+        echo "default-index";
+
+    }
+
+}
